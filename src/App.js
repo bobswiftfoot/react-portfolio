f7218d9917afa53397406cda49f42f506511285e
bobswiftfoot <natepfau@yahoo.com>
2021-09-19 19:25:21 -0500

    Changed NavBar to react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
 import Header from './components/Header';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import ContactForm from './components/Contact';
 import Resume from './components/Resume';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
 function App()
 {
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,23 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Header(props)
 {
     const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
     return (
         <section key="Header">
-            <nav className="navbar navbar-expand-lg sticky-top">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="./index.html">Nathan Pfau</a>
-                    <button className="navbar-toggler justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            {tabs.map(tab => (
-                                    <a
-                                        key={tab}
-                                        href={'#' + tab.toLowerCase()}
-                                        onClick={() => props.handlePageChange(tab)}
-                                        className={
-                                            props.currentPage === tab ? 'nav-link active' : 'nav-link'
-                                        }
-                                    >
-                                        {tab}
-                                    </a>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </nav>
+            <Navbar className="nav container-fluid" expand="lg" sticky="top">
+                <Navbar.Brand className="ms-1" bsPrefix="nav-brand">Nathan Pfau</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                    {tabs.map(tab => (
+                        <Nav.Link href={'#' + tab.toLowerCase()} onClick={() => props.handlePageChange(tab)} 
+                            active={ props.currentPage === tab} >{tab}</Nav.Link>
+                    ))}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </section>
     );
 }
